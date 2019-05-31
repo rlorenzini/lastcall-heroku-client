caec6cc45bdbcdce258e1010ba033d2309ba2ea9
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-05-31 14:57:07 -0500

    stuff

---
+++ build/precache-manifest.e157a94a55292c522eaab98c0f00facf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887ab3c354aedd0069acc28802b471b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f46694529ded0542e168",
+    "url": "/static/css/main.94097d8a.chunk.css"
+  },
+  {
+    "revision": "820a8789bfd61d3c6eb5",
+    "url": "/static/js/2.59a61812.chunk.js"
+  },
+  {
+    "revision": "f46694529ded0542e168",
+    "url": "/static/js/main.0404b385.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
